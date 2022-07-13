e5b20b9f96b6ce33904ad30ddd99139c7096ce3b
Alena Maletkina <aly0mal99@gmail.com>
2022-07-13 10:47:23 +0100

    post review fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,29 @@
-let editButton = document.querySelector('.profile__edit-btn');
+let editButton = document.querySelector('.profile__btn_type_edit');
 let closeButton = document.querySelector('.popup__close-btn');
 let popup = document.querySelector('.popup');
 let submitButton = document.querySelector('.popup__submit-btn')
-let popupForm = document.querySelector('.popup__form');
-let inputName = document.querySelector('.popup__input-name');
-let inputJob = document.querySelector('.popup__input-job');
+let popupForm = document.querySelector('.popup__input');
+let inputName = document.querySelector('.popup__field_type_name');
+let inputJob = document.querySelector('.popup__field_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
 function openEdit() {
-  popup.classList.add("popup__opened")
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+  popup.classList.add("popup_opened");
 }
 function closeEdit() {
-  popup.classList.remove("popup__opened")
+  popup.classList.remove("popup_opened");
 }
 function formHandler(evt) {
   evt.preventDefault();
   let nameValue = inputName.value;
   let jobValue = inputJob.value;
-  profileName.firstChild.textContent = nameValue;
+  profileName.textContent = nameValue;
   profileJob.textContent = jobValue;
+  closeEdit();
 }
 editButton.addEventListener("click", openEdit)
 closeButton.addEventListener("click", closeEdit);
 popupForm.addEventListener('submit', formHandler);
-submitButton.addEventListener('click', formHandler);
